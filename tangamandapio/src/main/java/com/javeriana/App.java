30d6718ff779b7414aee186795442186d1412275
Alberto Vigna <betoveluis@gmail.com>
2022-10-24 20:08:51 -0500

    Comunicación entre interfaz inicial e interfaz planificador.

--- tangamandapio/src/main/java/com/javeriana/App.java
+++ tangamandapio/src/main/java/com/javeriana/App.java
@@ -16,23 +16,21 @@ public class App extends Application {
     private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+    public void start(Stage stage) {
+        try {
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+            Parent root = FXMLLoader.load(getClass().getResource("primary.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }
