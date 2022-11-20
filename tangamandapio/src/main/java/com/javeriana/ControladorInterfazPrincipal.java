541c1913a713142fd7e9632c7a1741ad0e230efc
Alberto Vigna <betoveluis@gmail.com>
2022-11-20 15:47:25 -0500

    Update ControladorInterfazPrincipal.java

--- tangamandapio/src/main/java/com/javeriana/ControladorInterfazPrincipal.java
+++ tangamandapio/src/main/java/com/javeriana/ControladorInterfazPrincipal.java
@@ -1,15 +1,13 @@
 package com.javeriana;
 
-import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class ControladorInterfazPrincipal
 {
