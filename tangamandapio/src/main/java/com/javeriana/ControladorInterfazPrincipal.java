0a71986409cbe7cf7ff20bc5e5269d8021ed8222
Alberto Vigna <betoveluis@gmail.com>
2022-11-21 19:06:38 -0500

    Arreglo de todo lo que se daño jajajaja

--- tangamandapio/src/main/java/com/javeriana/ControladorInterfazPrincipal.java
+++ tangamandapio/src/main/java/com/javeriana/ControladorInterfazPrincipal.java
@@ -10,14 +10,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
-<<<<<<< HEAD:tangamandapio/src/main/java/com/javeriana/PrimaryController.java
-public class PrimaryController {
-=======
 public class ControladorInterfazPrincipal
 {
->>>>>>> d9c9780bd51a799074056a505454df57b7f8ff89:tangamandapio/src/main/java/com/javeriana/ControladorInterfazPrincipal.java
     private Stage stage;
     private Scene scene;
+    private Parent root;
 
     @FXML
     void PasarAInterfazPlanificador(ActionEvent event) throws IOException
@@ -30,32 +27,20 @@ public class ControladorInterfazPrincipal
     }
 
     @FXML
-<<<<<<< HEAD:tangamandapio/src/main/java/com/javeriana/PrimaryController.java
-    void PasarAInterfazInicial(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("primary.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-=======
     void PasarAInterfazCartero(ActionEvent event) throws IOException
     {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("InterfazCartero.fxml")));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
->>>>>>> d9c9780bd51a799074056a505454df57b7f8ff89:tangamandapio/src/main/java/com/javeriana/ControladorInterfazPrincipal.java
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
     @FXML
-<<<<<<< HEAD:tangamandapio/src/main/java/com/javeriana/PrimaryController.java
-    void PasarAInterfazPlanificador(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("secondary.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-=======
     void PasarAInterfazReporte(ActionEvent event) throws IOException
     {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("InterfazReporte.fxml")));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
->>>>>>> d9c9780bd51a799074056a505454df57b7f8ff89:tangamandapio/src/main/java/com/javeriana/ControladorInterfazPrincipal.java
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
