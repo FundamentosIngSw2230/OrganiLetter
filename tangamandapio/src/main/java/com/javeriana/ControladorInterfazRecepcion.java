161fd04166b7cb7575ad078952f37858f55f9b79
Alberto Vigna <betoveluis@gmail.com>
2022-11-21 23:14:05 -0500

    Terminada extración de datos de Registrar una carta

--- tangamandapio/src/main/java/com/javeriana/ControladorInterfazRecepcion.java
+++ tangamandapio/src/main/java/com/javeriana/ControladorInterfazRecepcion.java
@@ -69,7 +69,133 @@ public class ControladorInterfazRecepcion
     @FXML
     void RegistrarCartaRecepcion(ActionEvent event)
     {
+        // Si la carta es de envio express o no
+        boolean esExpress;
+
         System.out.println("Holaa");
+        // Obtener ID carta
+        try
+        {
+            int idCarta = Integer.parseInt(registrarCartaID.getText());
+        }
+        catch (NumberFormatException e) {
+            textoNotificacionesRecepcion.setText("El ID es incorrecto");
+            return;
+        }
+
+        // Obtener fecha de deposito
+        try
+        {
+            String fechaDeDeposito = registrarCartaFechaDeDeposito.getText();
+        }
+        catch (Exception e) {
+            textoNotificacionesRecepcion.setText("La fecha de deposito es incorrecta");
+        }
+
+        // Obtener fecha de entrega
+        try
+        {
+            String fechaDeEntrega = registrarCartaFechaDeEntrega.getText();
+        }
+        catch (Exception e) {
+            textoNotificacionesRecepcion.setText("La fecha de entrega es incorrecta");
+        }
+
+        // Obtener calle - direccion de recogida
+        try
+        {
+            int calleRecogida = Integer.parseInt(registrarCartaCalleRecogida.getText());
+        }
+        catch (NumberFormatException e) {
+            textoNotificacionesRecepcion.setText("La calle de recogida es incorrecta");
+            return;
+        }
+
+        // Obtener carrera - direccion de recogida
+        try
+        {
+            int carreraRecogida = Integer.parseInt(registrarCartaCarreraRecogida.getText());
+        }
+        catch (NumberFormatException e) {
+            textoNotificacionesRecepcion.setText("La carrera de recogida es incorrecta");
+            return;
+        }
+
+        // Obtener calle - direccion de entrega
+        try
+        {
+            int calleEntrega = Integer.parseInt(registrarCartaCalleEntrega.getText());
+        }
+        catch (NumberFormatException e) {
+            textoNotificacionesRecepcion.setText("La calle de entrega es incorrecta");
+            return;
+        }
+
+        // Obtener carrera - direccion de entrega
+        try
+        {
+            int carreraEntrega = Integer.parseInt(registrarCartaCarreraEntrega.getText());
+        }
+        catch (NumberFormatException e) {
+            textoNotificacionesRecepcion.setText("La calle de entrega es incorrecta");
+            return;
+        }
+
+        // Obtener estado de la carta
+        // Este es para comprobar si esta bien ingresado o no
+        String estado = registrarCartaEstadoCarta.getText();
+
+        // Utilizar este
+        String estadoFinal;
+
+        switch (estado)
+        {
+            case ("En buzon"):
+            {
+                estadoFinal = "EnBuzon";
+                break;
+            }
+
+            case ("En oficina"):
+            {
+                estadoFinal = "EnOficina";
+                break;
+            }
+
+            case ("En Ruta de Recogida"):
+            {
+                estadoFinal = "EnRutaRecogida";
+                break;
+            }
+
+            case ("En Ruta de Entrega"):
+            {
+                estadoFinal = "EnRutaEntrega";
+                break;
+            }
+
+            case ("Entregada"):
+            {
+                estadoFinal = "Entregada";
+                break;
+            }
+
+            default:
+            {
+                textoNotificacionesRecepcion.setText("Estado ingresado de forma incorrecta");
+            }
+        }
+
+
+        // Obtener si es express o no
+        if(registrarCartaExpressNO.isSelected())
+        {
+            esExpress = false;
+        }
+        else
+        {
+            esExpress = true;
+        }
     }
 
     // Buscar carta
