d7812a6de024b9aafb86fcdeb87195609123193c
Alberto Vigna <betoveluis@gmail.com>
2022-11-21 21:51:22 -0500

    Atributos colocados en interfaz "recepción" e inicializados en controlador

--- tangamandapio/src/main/java/com/javeriana/ControladorInterfazRecepcion.java
+++ tangamandapio/src/main/java/com/javeriana/ControladorInterfazRecepcion.java
@@ -6,7 +6,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,6 +21,41 @@ public class ControladorInterfazRecepcion
     private ImageView logoAsereje;
 
     @FXML
+    private TextField registrarCartaCalleEntrega;
+
+    @FXML
+    private TextField registrarCartaCalleRecogida;
+
+    @FXML
+    private TextField registrarCartaCarreraEntrega;
+
+    @FXML
+    private TextField registrarCartaCarreraRecogida;
+
+    @FXML
+    private TextField registrarCartaEstadoCarta;
+
+    @FXML
+    private RadioButton registrarCartaExpressNO;
+
+    @FXML
+    private RadioButton registrarCartaExpressSI;
+
+    @FXML
+    private TextField registrarCartaFechaDeDeposito;
+
+    @FXML
+    private TextField registrarCartaFechaDeEntrega;
+
+    @FXML
+    private TextField registrarCartaID;
+
+    @FXML
+    private Text textoNotificacionesRecepcion;
+
+
+    // Pasar a la interfaz principal
+    @FXML
     void PasarAInterfazInicialDesdePlanificador(ActionEvent event) throws IOException
     {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("InterfazPrincipal.fxml")));
@@ -26,4 +64,7 @@ public class ControladorInterfazRecepcion
         stage.setScene(scene);
         stage.show();
     }
+
+    // Registrar carta
+
 }
