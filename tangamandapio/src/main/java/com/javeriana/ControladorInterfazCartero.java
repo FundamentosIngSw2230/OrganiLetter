b3cba0faf9e143c08d9eda9a6f2451994813e2c0
Alberto Vigna <betoveluis@gmail.com>
2022-11-22 00:21:12 -0500

    Interfaz de cartero terminada :D

--- tangamandapio/src/main/java/com/javeriana/ControladorInterfazCartero.java
+++ tangamandapio/src/main/java/com/javeriana/ControladorInterfazCartero.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +15,13 @@ import java.util.Objects;
 
 public class ControladorInterfazCartero
 {
+    @FXML
+    private Text textoNotificacionesCartero;
+
+    @FXML
+    private TextArea vizualizarRutaCartero;
+
+
     // Pasar a la interfaz principal
     @FXML
     void PasarAInterfazInicialDesdeCartero(ActionEvent event) throws IOException
@@ -24,4 +33,22 @@ public class ControladorInterfazCartero
         stage.show();
     }
 
+    @FXML
+    void botonTerminarTurno(ActionEvent event)
+    {
+        // Aqui se pondria que el cartero ya esta desocupado
+        System.out.println("Boton terminar turno");
+
+        textoNotificacionesCartero.setText("Turno completado");
+    }
+
+    @FXML
+    void buscarRutaCartero(ActionEvent event)
+    {
+        String rutaAMostrar = "null"; // Cargar la ruta a ese string.
+
+        // Aqui se le mostraria la ruta al cartero
+        vizualizarRutaCartero.setText(rutaAMostrar);
+        textoNotificacionesCartero.setText("La ruta ha sidi cargada exitosamente");
+    }
 }
