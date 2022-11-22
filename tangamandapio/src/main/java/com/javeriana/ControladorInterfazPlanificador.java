9512036e085b403864e3dec9dd9f8da2fabe8487
Alberto Vigna <betoveluis@gmail.com>
2022-11-22 01:44:36 -0500

    Interfaz planificador terminada :D

--- tangamandapio/src/main/java/com/javeriana/ControladorInterfazPlanificador.java
+++ tangamandapio/src/main/java/com/javeriana/ControladorInterfazPlanificador.java
@@ -6,8 +6,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -18,7 +20,28 @@ import java.util.Objects;
 public class ControladorInterfazPlanificador {
 
     @FXML
+    private RadioButton actualizarCartaEnBuzon;
+
+    @FXML
+    private RadioButton actualizarCartaEnOficina;
+
+    @FXML
+    private RadioButton actualizarCartaEnRutaRecogida;
+
+    @FXML
+    private RadioButton actualizarCartaEntregada;
+
+    @FXML
+    private RadioButton actualizarCartaRutaEntregada;
+
+    @FXML
+    private ToggleGroup estadoDeLaCarta;
+
+    @FXML
     private ImageView imageViewLogoPlanificador;
+
+    @FXML
+    private TextArea textAreaRutaGenerada;
 
     @FXML
     private TextField textoIdCartaOrdenar;
@@ -27,13 +50,13 @@ public class ControladorInterfazPlanificador {
     private TextField textoIdCarteroAsignarRuta;
 
     @FXML
-    private TextArea textAreaRutaGenerada;
-
-    @FXML
     private TextField textoIdCarteroBuzones;
 
     @FXML
     private Text textoNotificacionesPlanificador;
+
+    @FXML
+    private TextField actualizarCartaID;
 
     // Pasar a la interfaz principal
     @FXML
@@ -50,11 +73,53 @@ public class ControladorInterfazPlanificador {
 
 
     // ----------------------------------------------------------------
-    // GENERAR REPORTE
+    // ACTUALIZAR CARTA
     @FXML
     void accionarBotonActualizarCarta(ActionEvent event)
     {
+        // Este es el estado de la carta
+        String estadoDeLaCarta = null;
+        // Este es el ID de la carta
+        int idCartaAActualizar = 0;
+
         System.out.println("Holaa, este es el boton de actualizar carta");
+
+        // Tomar el ID de la carta a actualizar
+        try
+        {
+            idCartaAActualizar = Integer.parseInt(actualizarCartaID.getText());
+            // EXTRAER AQUI EL ID DE LA CARTA AQUI
+        }
+        catch (NumberFormatException e)
+        {
+            textoNotificacionesPlanificador.setText("El ID es incorrecto");
+        }
+
+        // Tomar el estado a actualizar
+        if(actualizarCartaEnBuzon.isSelected())
+        {
+            estadoDeLaCarta = "EnBuzon";
+        }
+        if (actualizarCartaEnOficina.isSelected())
+        {
+            estadoDeLaCarta = "EnOficina";
+        }
+        if(actualizarCartaEnRutaRecogida.isSelected())
+        {
+            estadoDeLaCarta = "EnRutaRecogida";
+        }
+        if(actualizarCartaEntregada.isSelected())
+        {
+            estadoDeLaCarta = "EnRutaEntrega";
+        }
+        if(actualizarCartaEntregada.isSelected())
+        {
+            estadoDeLaCarta = "Entregada";
+        }
+        // SACAR EL ESTADO DE LA CARTA AQUI
+
+        System.out.println("El ID de la carta es: " + idCartaAActualizar);
+        System.out.println("El estado seleccionado es: " + estadoDeLaCarta);
     }
 
     // ----------------------------------------------------------------
